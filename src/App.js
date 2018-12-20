15aadb98d66950bb2bf813209660b036d4689fec
Angela Castagno <oesed16@gmail.com>
2018-12-20 00:17:29 -0500

    Reto Repetidor React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
+  repeater = event => {
+    this.setState({
+      value: event.target.value
+    });
+  }
+  
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.repeater} />
+        <p className="repeater">{this.state.value}</p>
       </div>
     );
   }
